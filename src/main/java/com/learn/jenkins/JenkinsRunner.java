9d00c2080fc969af79a7b36ce8f291af93f14bac
Gomding <backchida@gmail.com>
2021-07-13 00:06:48 +0900

    feat : add somethind todo

--- src/main/java/com/learn/jenkins/JenkinsRunner.java
+++ src/main/java/com/learn/jenkins/JenkinsRunner.java
@@ -10,5 +10,7 @@ public class JenkinsRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("do something");
+        System.out.println("한번 일할거임");
+        System.out.println("두번 일할거임");
     }
 }
