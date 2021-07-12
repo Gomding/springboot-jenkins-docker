61963734345675a17e088ba2d7fb4f624abcb48e
Gomding <backchida@gmail.com>
2021-07-12 19:18:42 +0900

    feat : add something

---
+++ src/test/java/com/learn/jenkins/JenkinsTestApplicationTest.java
@@ -0,0 +1,11 @@
+package com.learn.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JenkinsTestApplicationTest {
+    @Test
+    public void context() {
+    }
+}
