fd5223342a12c871b3b1f1f43133b15530c50abb
Gomding <backchida@gmail.com>
2021-07-14 02:16:58 +0900

    feat: stop ddd2

--- backend/src/main/java/com/learn/jenkins/JenkinsRunner.java
+++ backend/src/main/java/com/learn/jenkins/JenkinsRunner.java
@@ -15,11 +15,8 @@ public class JenkinsRunner implements ApplicationRunner {
         System.out.println("세번 일할거임");
 
         System.out.println("새로운 버전 SNAPSHOT-1.0.1 도 배포했다고");
-
         System.out.println("아니 push만 해도 배포가 된다고?");
-
         System.out.println("다시하자");
-
         System.out.println("이번엔 되겠지");
 
         System.out.println("ddd");
